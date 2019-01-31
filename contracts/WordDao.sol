8b493b30376cf5e4b2e57b3e78a519e6f2e10a1c
Dennison Bertram <dennison@dennisonbertram.com>
2019-01-31 12:12:48 -0300

    Remove session data to start over.

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -14,9 +14,57 @@ contract WordDao is Initializable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
 
     event wordAdded(address _adder, string word);
 
-    function initialize() initializer public {
-        _totalWords = 0;
+     function initialize(
+        string memory name, string memory symbol, uint8 decimals, uint256 initialSupply, address initialHolder,
+        address[] memory minters, address[] memory pausers
+    ) public initializer {
+        ERC20Detailed.initialize(name, symbol, decimals);
+
+        // Mint the initial supply
+        _mint(initialHolder, initialSupply);
+
+        // Initialize the minter and pauser roles, and renounce them
+        ERC20Mintable.initialize(address(this));
+        _removeMinter(address(this));
+
+        ERC20Pausable.initialize(address(this));
+        _removePauser(address(this));
+
+        // Add the requested minters and pausers (this can be done after renouncing since
+        // these are the internal calls)
+        for (uint256 i = 0; i < minters.length; ++i) {
+            _addMinter(minters[i]);
+        }
+
+        for (uint256 i = 0; i < pausers.length; ++i) {
+            _addPauser(pausers[i]);
+        }
     }
+
+    function initialize(
+        string memory name, string memory symbol, uint8 decimals, address[] memory minters, address[] memory pausers
+    ) public initializer {
+        ERC20Detailed.initialize(name, symbol, decimals);
+
+        // Initialize the minter and pauser roles, and renounce them
+        ERC20Mintable.initialize(address(this));
+        _removeMinter(address(this));
+
+        ERC20Pausable.initialize(address(this));
+        _removePauser(address(this));
+
+        // Add the requested minters and pausers (this can be done after renouncing since
+        // these are the internal calls)
+        for (uint256 i = 0; i < minters.length; ++i) {
+            _addMinter(minters[i]);
+        }
+
+        for (uint256 i = 0; i < pausers.length; ++i) {
+            _addPauser(pausers[i]);
+        }
+    }
+
+
 
     function setWord(string memory word) public payable{
     //require(msg.value == 1 ether);
