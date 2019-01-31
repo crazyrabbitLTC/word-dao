90ed37d7976089e7946f29cefe3a3acd446c85bd
Dennison Bertram <dennison@dennisonbertram.com>
2019-01-31 12:25:41 -0300

    Update truffle-config.js for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,42 @@
-'use strict';
+
+"use strict";
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var secrets = require("./secrets.js");
+var apiRinkeby = secrets.apiRinkeby;
+var apiMainnet = secrets.apiMainnet;
+var mnemonic = secrets.mnemonic;
+
+console.log("Mnemonic is: ", mnemonic);
 
 module.exports = {
   networks: {
     local: {
-      host: 'localhost',
+      host: "localhost",
       port: 9545,
       gas: 5000000,
       gasPrice: 5e9,
-      network_id: '*'
+      network_id: "*"
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          apiMainnet
+        );
+      },
+      gas: 200000000,
+      network_id: 1
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          apiRinkeby
+        );
+      },
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: 3
     }
   },
   compilers: {
