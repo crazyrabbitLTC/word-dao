067b043323d87452e9d3ce50a144b5f9fe688be5
Dennison Bertram <dennison@dennisonbertram.com>
2019-01-31 08:32:34 -0300

    New Compiler version

--- truffle-config.js
+++ truffle-config.js
@@ -9,5 +9,10 @@ module.exports = {
       gasPrice: 5e9,
       network_id: '*'
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.20"  // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ }
 };
