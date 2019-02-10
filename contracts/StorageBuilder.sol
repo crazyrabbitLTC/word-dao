d1a78e391565906839a3ba07976bb50ba1d84652
Dennison Bertram <dennison@dennisonbertram.com>
2019-02-09 19:15:37 -0500

    Add language array to storage builder.

--- contracts/StorageBuilder.sol
+++ contracts/StorageBuilder.sol
@@ -5,13 +5,15 @@ import "../contracts/Storage.sol";
 contract storageBuilder {
     
     address[] public storageLocations;
+    string[] public languages;
     
     function getStorageCount() public view returns(uint256){
         return storageLocations.length;
     }
     
-    function deployStorage(string memory _language) public returns(address){
+    function deployStorage(string calldata _language) external returns(address){
         WordStorage c = new WordStorage(_language);
+        languages.push(_language);
         storageLocations.push(address(c));
         return address(c);
     }
