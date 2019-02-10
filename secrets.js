9c3faea5c8c9bbffd0cf95b27f03b5467474ab39
Dennison Bertram <dennison@dennisonbertram.com>
2019-02-09 19:04:43 -0500

    Minor change

---
+++ secrets.js
@@ -0,0 +1,7 @@
+const config = {
+  apiRinkeby: 'https://rinkeby.infura.io/v3/e9ad5000cd7b443e9b9b5fbc9996821d',
+  apiMainnet: 'https://mainnet.infura.io/v3/e9ad5000cd7b443e9b9b5fbc9996821d',
+  mnemonic: 'wood reform despair biology hurry daughter return rate armor dismiss industry dutch',
+}
+
+module.exports = config;
