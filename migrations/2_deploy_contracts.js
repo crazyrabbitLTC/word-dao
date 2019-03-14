d2504c8fd46e20daa2b781007943293b94de9909
Dennison Bertram <dennison@dennisonbertram.com>
2019-03-14 09:57:22 -0400

    Working Test Suite

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const StorageBuilder = artifacts.require("StorageBuilder");
+const Storage = artifacts.require("Storage");
 
 
 module.exports = function(deployer) {
   deployer.deploy(StorageBuilder);
+  deployer.deploy(Storage, "English");
 };
