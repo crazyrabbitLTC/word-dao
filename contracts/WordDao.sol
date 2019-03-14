56b625917b654e58c4ede2785299a0d232fafa46
Dennison Bertram <dennison@dennisonbertram.com>
2019-03-14 14:06:27 -0400

    Use only truffle solidit 0.5.0 (zos is behind)

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -2,20 +2,24 @@ pragma solidity ^0.5.0;
 
 import "zos-lib/contracts/Initializable.sol";
 import "openzeppelin-eth/contracts/token/ERC20/ERC20.sol";
+//import "zos-lib/contracts/application/App.sol";
 import "./Storage.sol";
 import "./StorageBuilder.sol";
 
 contract WordDao is Initializable {
 
+    //App public zosApp;
     ERC20 public erc20;
-    Storage public dataBank;
+    Storage[] public dataBank;
     StorageBuilder public dataBankBuilder;
 
+
     //this function should create an ERC20 and databankbuilder. I should not need to enter the addresses. 
-    function initialize(ERC20 _erc20, Storage _dataBank, StorageBuilder _dataBankBuilder) initializer public {
-        erc20 = _erc20;
-        dataBank = _dataBank;
-        dataBankBuilder = _dataBankBuilder;
+    function initialize() initializer public {
+
+        //zosApp = new App();
+        erc20 = new ERC20();
+        dataBankBuilder = new StorageBuilder();
     }
 
 
