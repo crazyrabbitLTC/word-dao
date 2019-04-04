f18e5c9248cf3a53078a22a84f485d9e3991cf36
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-03 20:03:00 -0400

    Started thinking of how to organize WordStorages

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -14,15 +14,16 @@ contract Manager is Initializable {
         address _daoStorage;
     }
 
-   WordDao[] public wordDaos;
+   WordDao thisWordDao;
     
+
     function createStorage(string memory _language, string memory _name, string memory _symbol, 
     uint8 _decimals, uint256 _initialSupply, address _initialHolder, address[] memory _minters, 
     address[] memory _pausers) 
     public{
         
     StandaloneERC20 erc20 = new StandaloneERC20();
-    erc20.initialize(_name, _symbol,_decimals, _initialSupply, _initialHolder,_minters,_pausers);
+    erc20.initialize(_name, _symbol,_decimals, _initialSupply, address(this),_minters,_pausers);
 
     DaoStorage daoStorage = new DaoStorage();
     daoStorage.initialize(_language);
@@ -31,8 +32,20 @@ contract Manager is Initializable {
     wordDao._language = _language;
     wordDao._token = address(erc20);
     wordDao._daoStorage = address(daoStorage);
-    
+
+    //thisWordDao = wordDao;
+
     emit daoStorageCreated(wordDao._language, wordDao._token,wordDao._daoStorage);
     }
 
+    function addWord(string memory _word) public {
+
+    }
+
+    function getBalance() public returns(uint256){
+        return address(this).balance;
+    }
+
+    function () payable external {
+    }
 }
