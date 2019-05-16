5049bc00d09adcc4635e10789af9b517737378ef
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-16 13:17:42 -0400

    Promisify the file loading line by line.

--- signServer/getWords.js
+++ signServer/getWords.js
@@ -1,35 +1,34 @@
-const { createReadStream } = require('fs');
-const { createInterface } = require('readline');
+const { createReadStream } = require("fs");
+const { createInterface } = require("readline");
 const path = require("path");
 const file = path.join("words.txt");
 const words = new Map();
 
 let lineNumber = 0;
 
-(async function processLineByLine() {
+const processLineByLine = new Promise(function(resolve, reject) {
   try {
     const rl = createInterface({
       input: createReadStream(file),
       crlfDelay: Infinity
     });
 
-    rl.on('line', (line) => {
-      // Process the line.
+    rl.on("line", line => {
       words.set(lineNumber, line);
       lineNumber++;
-      
     });
 
-    rl.once('close', (stream) => {
-        console.log('File processed.');
-        console.log(`Map size: ${words.size}`);
-      });
-    //await once(rl, 'close');
-
-
+    rl.once("close", stream => {
+      resolve(words.size);
+    });
   } catch (err) {
-    console.error(err);
+    reject(err);
   }
-})();
+});
 
+const app = async () => {
+  const amount = await processLineByLine;
+  console.log("Total amount: ", amount);
+};
 
+app();
