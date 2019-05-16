dae21a670d69a1dcb380af3410ca9c7d51fd9882
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-16 14:06:37 -0400

    Add IPFS and Orbit DB

--- signServer/getWords.js
+++ signServer/getWords.js
@@ -1,34 +1,74 @@
 const { createReadStream } = require("fs");
 const { createInterface } = require("readline");
+const IPFS = require("ipfs");
+const OrbitDB = require("orbit-db");
+
 const path = require("path");
 const file = path.join("words.txt");
-const words = new Map();
 
-let lineNumber = 0;
+const getIPFS = async () => {
+  const ipfsOptions = {
+    EXPERIMENTAL: {
+      pubsub: true
+    }
+  };
+  const createIPFS = new Promise(function(resolve, reject) {
+    try {
+      const ipfs = new IPFS(ipfsOptions);
+      ipfs.on("error", e => reject(e));
+      ipfs.on("ready", async () => {
+        console.log("IPFS READY");
+        resolve(ipfs);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+  let output = createIPFS;
+  return output;
+};
+const processFile = async file => {
+  const processLineByLine = new Promise(function(resolve, reject) {
+    const words = new Map();
+    let lineNumber = 0;
+    try {
+      const rl = createInterface({
+        input: createReadStream(file),
+        crlfDelay: Infinity
+      });
 
-const processLineByLine = new Promise(function(resolve, reject) {
-  try {
-    const rl = createInterface({
-      input: createReadStream(file),
-      crlfDelay: Infinity
-    });
+      rl.on("line", async line => {
+        words.set(lineNumber, line);
+        lineNumber++;
+      });
 
-    rl.on("line", line => {
-      words.set(lineNumber, line);
-      lineNumber++;
-    });
+      rl.once("close", stream => {
+        resolve(words);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 
-    rl.once("close", stream => {
-      resolve(words.size);
-    });
-  } catch (err) {
-    reject(err);
-  }
-});
+  let output = await processLineByLine;
+  return output;
+};
 
-const app = async () => {
-  const amount = await processLineByLine;
-  console.log("Total amount: ", amount);
+const createOrbitDB = async (ipfs, databaseName) => {
+
+  const orbitdb = await OrbitDB.createInstance(ipfs);
+  // Create / Open a database
+  db = await orbitdb.keyvalue(databaseName);
+  await db.load();
+  return db;
+};
+
+const app = async db => {
+  const wordMap = await processFile(file);
+  console.log("Map size: ", wordMap.size);
+  const ipfs = await getIPFS();
+  const db = await createOrbitDB(ipfs, "DennisonsDatabase");
+  
 };
 
 app();
