8c0183c876682734cb91f77b923dd6219a02842e
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-07-18 14:13:12 -0400

    Example for OZ platform UI Bug

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import "zos-lib/contracts/Initializable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721MetadataMintable.sol";
+import "./WordStorage.sol";
 
 
 
@@ -22,19 +23,23 @@ contract WordDao is Initializable {
 // WithdrawDividends
 // externalcontract ExternalContract
 
-    ERC20Mintable public ERC20Token;
-    ERC721MetadataMintable public ERC721Token;
+    ERC20Mintable public erc20Token;
+    ERC721MetadataMintable public erc721Token;
+    WordStorage public wordStore;
 
     event ERC20TokenAttached(address ERC20Token);
     event ERC721TokenAttached(address ERC721Token);
+    event WordStoreAttached(address WordStore);
 
-    function initialize (ERC20Mintable _ERC20Token, ERC721MetadataMintable _ERC721Token) public initializer {
-        ERC20Token = _ERC20Token;
-        ERC721Token = _ERC721Token;
+    function initialize (ERC20Mintable _erc20Token, ERC721MetadataMintable _erc721Token, WordStorage _wordStore) public initializer {
+        erc20Token = _erc20Token;
+        erc721Token = _erc721Token;
+        wordStore = _wordStore;
+        
 
-        emit ERC20TokenAttached(address(ERC20Token));
-        emit ERC721TokenAttached(address(ERC721Token));
-
+        emit ERC20TokenAttached(address(erc20Token));
+        emit ERC721TokenAttached(address(erc721Token));
+        emit WordStoreAttached(address(wordStore));
     }
 
 
